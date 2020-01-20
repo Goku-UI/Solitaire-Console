4b92853a6e844e77956c67d656c957b603c70470
Gaël Fortier <fortier267@gmail.com>
2020-01-20 16:20:49 -0500

    Update simple_list.h

--- simple_list.h
+++ simple_list.h
@@ -1,6 +1,15 @@
 //--------------
-// The list is a single-chained list.
+// The list is a single-chained list. (aka a stack)
 //--------------
 
 
 struct slnode
+{
+	void* pointer;
+	slnode* next;
+};
+
+slnode* NewSLNode(void* pointer, slnode* next)
+{
+	
+}
